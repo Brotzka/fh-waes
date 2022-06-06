7997fedd74660cb101716e104d85f0a11158c535
Fabian Hagen <fabianhagen87@gmail.com>
2022-06-06 21:15:43 +0000

    Login Route erstellt

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\LoginRequest;
 use Illuminate\Http\Request;
 use App\Http\Requests\User\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -28,10 +31,43 @@ class UserController extends Controller
         $token = $user->createToken('UserToken');
 
         return response()->json([
-            'success' => 'true',
+            'success' => true,
             'message' => 'registered',
             'api_token' => $token->plainTextToken,
             'user' => $user
         ]);
+    }
+
+    /**
+     * Logs a user into the application
+     *
+     * @param LoginRequest $request
+     * @return JsonResponse
+     */
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $credentials = $request->only(['email','password']);
+
+        if(Auth::attempt($credentials)) {
+            // success
+
+            /** @var User $user */
+            $user = Auth::user();
+            $user->tokens()->delete();
+            $token = $user->createToken('UserToken');
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Authenticated',
+                'user' => $user,
+                'api_token' => $token->plainTextToken
+            ], Response::HTTP_OK);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'No user found with these credentials!'
+
+            ], Response::HTTP_UNAUTHORIZED);
+        }
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::controller(UserController::class)->prefix('user')->name('user')->group(function() {
+Route::controller(UserController::class)->prefix('users')->name('users')->group(function() {
     Route::post('', 'register');
+    Route::post('login', 'login');
 });
