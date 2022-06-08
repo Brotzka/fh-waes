6224575cf0cf01e0bbc1dc8ceb69957de1d50be6
Fabian Hagen <fabianhagen87@gmail.com>
2022-06-08 11:50:16 +0000

    Register Form fertig

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Axios from 'axios'
 
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
@@ -8,6 +9,15 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/scss/app.scss'
 import router from './router'
 import store from './store'
+
+console.log(process.env.VUE_APP_API_BASE_URL)
+console.log(process.env.VUE_APP_TEST) //
+
+const axios = Axios.create({
+  baseURL: process.env.VUE_APP_API_BASE_URL + "api/"
+})
+process.env.GITPOD_WORKSPACE_ID
+Vue.prototype.$http = axios
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
--- frontend/src/views/auth/RegisterView.vue
+++ frontend/src/views/auth/RegisterView.vue
@@ -1,9 +1,54 @@
 <template>
-  <b-container>
-    <div class="home">
+  <b-container id="register-view">
+    <b-row class="home">
+      <b-col cols="12" md="6" offset-md="3">
         <h1>Registrieren</h1>
 
-    </div>
+          <p>
+            Hier kannst du dich für einen Account registieren.
+          </p>
+
+        <b-form @submit.prevent="registerUser">
+        
+          <b-form-group
+            id="user-name"
+            label="Dein Nutzername"
+            label-for="name"
+          >
+            <b-form-input id="name" v-model="user.name" trim></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="user-email"
+            label="Deine Mailadresse"
+            label-for="email"
+          >
+            <b-form-input id="email" type="email" v-model="user.email" trim></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="user-email"
+            label="Dein Passwort"
+            label-for="password"
+          >
+            <b-form-input id="password" type="password" v-model="user.password" trim></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="user-email"
+            label="Bitte gebe dein Passwort erneut ein"
+            label-for="password-confirmation"
+          >
+            <b-form-input id="password-confirmation" type="password" v-model="user.password_confirmation" trim></b-form-input>
+          </b-form-group>
+
+          <b-form-group>
+            <b-button type="submit" variant="primary" class="mt-3">Registrieren</b-button>
+          </b-form-group>
+
+        </b-form>
+      </b-col>
+    </b-row>
   </b-container>
   
 </template>
@@ -12,5 +57,27 @@
 
 export default {
   name: 'RegisterView',
+  data() {
+    return {
+      user: {
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: ""
+      }
+    }
+  },
+  methods: {
+    registerUser() {
+      console.log(this.user)
+      this.$http.post('users/register', this.user)
+      .then(resp => {
+        console.log(resp)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
