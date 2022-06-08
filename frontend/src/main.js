a9433617039e25721b40971cda8edff12f3c996e
Fabian Hagen <fabianhagen87@gmail.com>
2022-06-08 09:50:10 +0000

    VueRouter und Vuex installiert

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,8 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/scss/app.scss'
+import router from './router'
+import store from './store'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
@@ -15,5 +17,7 @@ Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
