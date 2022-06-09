d3cba5152502639d9fbe465b1bdf180885f89bd4
Fabian Hagen <fabianhagen87@gmail.com>
2022-06-09 07:35:28 +0000

    URL angepasst

--- frontend/src/http.js
+++ frontend/src/http.js
@@ -4,23 +4,18 @@ import store from "./store"
 export const backendApi = () => {
   const http = axios.create({
     //baseURL: process.env.VUE_APP_API_BASE_URL + "api/",
-    baseURL: "https://8000-brotzka-fhwaes-ay4tgdrm36c.ws-eu47.gitpod.io/api/",
+    baseURL: "https://8000-brotzka-fhwaes-ciw7npuwzb9.ws-eu47.gitpod.io/api/",
     headers: {
       "Access-Control-Allow-Origin": "*"
     }
   });
 
   http.interceptors.request.use(function(config) {
-    console.log("Config", config)
-
     const api_token = store.getters.api_token
 
     if(api_token != null) {
       config.headers['Authorization'] = `Bearer ${api_token}`
     }
-
-    console.log(config)
-
     return config
   }, function(err) {
     return Promise.reject(err)
