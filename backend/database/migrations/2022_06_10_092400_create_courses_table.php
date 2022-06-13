577d35bb9277ee4198815a10769f0c26298cca08
Fabian Hagen <fabianhagen87@gmail.com>
2022-06-13 10:18:47 +0200

    Update 2022_06_10_092400_create_courses_table.php

--- backend/database/migrations/2022_06_10_092400_create_courses_table.php
+++ backend/database/migrations/2022_06_10_092400_create_courses_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->integer('teacher_id')->unsigned();
+            $table->unsignedBigInteger('teacher_id');
             $table->string('name');
             $table->text('description');
             $table->timestamp('start_date');
