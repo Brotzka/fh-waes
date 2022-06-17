20e1560ad569fba49b856332735c4b98026b74a0
Fabian Hagen <fabianhagen87@gmail.com>
2022-06-17 09:57:38 +0000

    Tabelle wird gerendert

--- frontend/src/components/courses/CoursesList.vue
+++ frontend/src/components/courses/CoursesList.vue
@@ -1,6 +1,23 @@
 <template>
     <div>
         <h2>Übersicht</h2>
+
+        <table v-if="courses.length > 0">
+            <tr>
+                <th>Name</th>
+                <th>Beschreibung</th>
+                <th>Startet am</th>
+                <th>Endet am</th>
+            </tr>
+            <tr v-for="course in courses" :key="course.id">
+                <td>{{ course.name }}</td>
+                <td>{{ course.description }}</td>
+                <td>{{ course.start_date }}</td>
+                <td>{{ course.end_date }}</td>
+            </tr>
+        </table>
+
+        
     </div>
 </template>
 
@@ -20,6 +37,7 @@ export default {
         this.$http.get('/courses/list')
             .then(response => {
                 console.log(response)
+                this.courses = response.data.data
             })
             .catch(error => {
                 console.error(error)
